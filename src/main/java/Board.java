5de4b1144fd61375b39be24155d1142aae0e61aa
eddientim <edward11r@yahoo.co.uk>
2023-03-06 13:46:38 +0000

    refactor to take static board size + add game rules to board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -6,28 +6,25 @@ import java.util.Set;
 
 public class Board {
 
-    private final int row;
-    private final int column;
-    private final int boardSize;
+    private final int boardSize = 3;
     private int occupiedBoxes;
-    Set<String> validCells = new HashSet<>();
 
-    public Board(int row, int column, int boardSize) {
-        this.row = row;
-        this.column = column;
-        this.boardSize = boardSize;
-    }
+    private char[][] board = new char[boardSize][boardSize];
+    private Set<String> validCells = new HashSet<>();
 
-    public void pieceOutOfBounds() {
-        if ((row > boardSize) || (column > boardSize)) {
-            throw new PieceOutOfBoundsException("Piece is outside board");
+    public Board() {
+        for (int row = 0; row < this.boardSize; row++) {
+            for (int column = 0; column < this.boardSize; column++) {
+                board[row][column] = ' ';
+                validCells.add(row+""+column);
+            }
         }
     }
 
-    public boolean markBox(char playerMark) {
-        char[][] board = new char[boardSize][boardSize];
+    public boolean markBox(char playerMark, int row, int column) {
+        pieceOutOfBounds(row, column);
 
-        if (board[row][column] != '\0') {
+        if (board[row][column] != ' ') {
             throw new BoxOccupiedException("Box already occupied.");
         } else {
             board[row][column] = playerMark;
@@ -36,20 +33,91 @@ public class Board {
         return true;
     }
 
-    private void initialBoard(char[][] board) {
+    private void pieceOutOfBounds(int row, int column) {
+        if ((row < 0 || row > boardSize) || (column < 0 || column > boardSize)) {
+            throw new PieceOutOfBoundsException("Piece is outside board");
+        }
+    }
+
+    public boolean hasWinner() {
+        return (checkRowsForWinner() || checkColumnsForWinner() || checkDiagonalsForWinner());
+    }
+
+    private boolean checkDiagonalsForWinner() {
+        return checkFirstDiagonal() || checkSecondDiagonal();
+    }
+
+    private boolean checkFirstDiagonal() {
+        char cell1 = board[0][0];
+        int rowColumn = 1;
+        while (rowColumn < boardSize) {
+            if (cell1 == ' ' || cell1 != board[rowColumn][rowColumn]) {
+                return false;
+            }
+            rowColumn++;
+        }
+        return true;
+    }
+
+    private boolean checkSecondDiagonal() {
+        char cell1 = board[0][2];
+        int row = 1;
+        int column = 1;
+        while (row < boardSize) {
+            if (cell1 == ' ' || cell1 != board[row][column]) {
+                return false;
+            }
+            row++;
+            column--;
+        }
+        return true;
+    }
+
+    private boolean checkRowsForWinner() {
         for (int row = 0; row < boardSize; row++) {
-            for (int column = 0; column < boardSize; column++) {
-                board[row][column] = ' ';
-                validCells.add(row + "" + column);
+            if (checkRow(row)) {
+                return true;
             }
         }
+        return false;
+    }
+
+    private boolean checkColumnsForWinner() {
+        for (int column = 0; column < boardSize; column++) {
+            if (checkColumn(column)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkRow(int row) {
+        char cell1 = board[row][0];
+        int column = 1;
+        while (column < boardSize) {
+            if (cell1 == ' ' || cell1 != board[row][column]) {
+                return false;
+            }
+            column++;
+        }
+        return true;
+    }
+
+    private boolean checkColumn(int column) {
+        char cell1 = board[0][column];
+        int row = 1;
+        while (row < boardSize) {
+            if (cell1 == ' ' || cell1 != board[row][column]) {
+                return false;
+            }
+            row++;
+        }
+        return true;
     }
 
     @Override
     public String toString() {
         return "Board{" +
-                "x=" + row +
-                ", y=" + column +
                 ", boardSize=" + boardSize +
                 ", occupiedBoxes=" + occupiedBoxes +
                 '}';
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -9,9 +9,9 @@ public class BoardTest {
     @Test
     public void shouldPlacePieceWithinOnBoard() {
 
-        Board board = new Board(1, 1, 3);
+        Board board = new Board();
 
-        boolean actual = board.markBox('O');
+        boolean actual = board.markBox('O', 1, 2);
 
         assertTrue(actual);
     }
@@ -19,20 +19,75 @@ public class BoardTest {
     @Test
     public void shouldThrowPieceOutOfBoundsExceptionWhenRowIsPlacedOffBoard() {
         assertThrows(PieceOutOfBoundsException.class, () ->
-                new Board(4, 1, 3).pieceOutOfBounds());
+                new Board().markBox('O', 4, 2));
+    }
+
+    @Test
+    public void shouldThrowPieceOutOfBoundsExceptionWhenNegativeNumber() {
+        assertThrows(PieceOutOfBoundsException.class, () ->
+                new Board().markBox('O', -1, -2));
     }
 
     @Test
     public void shouldThrowPieceOutOfBoundsExceptionWhenColumnIsPlacedOffBoard() {
         assertThrows(PieceOutOfBoundsException.class, () ->
-                new Board(1, 5, 3).pieceOutOfBounds());
+                new Board().markBox('X', 1, 4));
     }
 
     @Test
     public void shouldThrowOccupiedExceptionWhenPieceAlreadyExistsInBox() {
 
-        Board board = new Board(1, 1, 3);
-        assertThrows(BoxOccupiedException.class, () -> board.markBox('X'));
+        Board board = new Board();
+        board.markBox('O', 1, 1);
+        assertThrows(BoxOccupiedException.class, () -> board.markBox('X', 1, 1));
+    }
+
+    @Test
+    public void shouldCheckForRowWin() {
+        Board board = new Board();
+
+        board.markBox('0',2,0);
+        board.markBox('0',2,1);
+        board.markBox('0',2,2);
+
+        board.hasWinner();
+        assertTrue(board.hasWinner());
+    }
+
+    @Test
+    public void shouldCheckForColumnWin() {
+        Board board = new Board();
+
+        board.markBox('0',0,1);
+        board.markBox('0',1,1);
+        board.markBox('0',2,1);
+
+        board.hasWinner();
+        assertTrue(board.hasWinner());
+    }
+
+    @Test
+    public void shouldCheckForDiagonalWinTopLeftToBottom() {
+        Board board = new Board();
+
+        board.markBox('0',0,0);
+        board.markBox('0',1,1);
+        board.markBox('0',2,2);
+
+        board.hasWinner();
+        assertTrue(board.hasWinner());
+    }
+
+    @Test
+    public void shouldCheckForDiagonalWinTopRightToLeft() {
+        Board board = new Board();
+
+        board.markBox('0',0,2);
+        board.markBox('0',1,1);
+        board.markBox('0',2,0);
+
+        board.hasWinner();
+        assertTrue(board.hasWinner());
     }
 }
 
